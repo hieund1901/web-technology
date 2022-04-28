e43232cbffd053d111323e41cfb1a00a51ba05f2
TuLN194396 <97429355+TuLN194396@users.noreply.github.com>
2022-04-28 21:58:58 +0700

    Update Exercise.php

--- Lab2/Exercise.php
+++ Lab2/Exercise.php
@@ -8,6 +8,8 @@
         print("<br>Helo, $name");
         print("<br>You are studying at $class, $univer");
         print("<br> Your hobby is:");
+        print("<br>1. Like Travel");
+        print("<br>1. Like Study");
     ?>
 </body>
 
